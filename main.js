3ba1841942e920314be4d94dedc4040db068fcba
ikasamd <ikasam.d@gmail.com>
2020-08-25 05:31:57 +0900

    色々

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-test
+var img = new Image();    //画像オブジェクト作成
+img.src = "img/316x384.png";  //写真のパスを指定する
+
+function draw1(){
+    var ctx = document.getElementById("canvas").getContext("2d");
+    ctx.drawImage(img,0,0);
+}
+function main(){
+    draw1();
+}
