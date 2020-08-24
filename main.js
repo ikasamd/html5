e5a9ad312ab4b2f48669e3f5dc72186a4edaaa32
ikasamd <ikasam.d@gmail.com>
2020-08-25 06:25:17 +0900

    add gamepad

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
 var img = new Image();    //画像オブジェクト作成
 img.src = "img/316x384.png";  //写真のパスを指定する
 
-function draw1(){
+function draw1() {
     var ctx = document.getElementById("canvas").getContext("2d");
-    ctx.drawImage(img,0,0);
+    ctx.drawImage(img, 0, 0);
 }
-function main(){
+
+function init(){
+    if (window.GamepadEvent) {
+        // ゲームパッドを接続すると実行されるイベント
+        window.addEventListener("gamepadconnected", function (e) {
+            console.log("ゲームパッドが接続された");
+            console.log(e.gamepad);
+        });
+        // ゲームパッドの接続を解除すると実行されるイベント
+        window.addEventListener("gamepaddisconnected", function (e) {
+            console.log("ゲームパッドの接続が解除された");
+            console.log(e.gamepad);
+        });
+    }else{
+        console.log("GamepadEvent未対応");
+    }
+}
+function main() {
+    console.log("main:start")
+    init();
     draw1();
+    console.log("main:end")
 }
