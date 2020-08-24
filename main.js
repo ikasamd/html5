da1c15e6df2dd43755fd9738b1767e841d77299a
ikasamd <ikasam.d@gmail.com>
2020-08-25 06:58:04 +0900

    up

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-var outsite = "https://rawgithub.com/ikasamd/html5/master/";
 var img = new Image();    //画像オブジェクト作成
-img.src = outsite + "img/316x384.png";  //写真のパスを指定する
+img.src = "img/316x384.png";  //写真のパスを指定する
 
 function draw1() {
     var ctx = document.getElementById("canvas").getContext("2d");
